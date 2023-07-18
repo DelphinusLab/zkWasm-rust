4561f907a0ab56ae49c2bffd52c17de3faee540d
sinka <xgao@zoyoe.com>
2023-07-18 13:24:41 +1000

    fix unit tests

--- test.sh
+++ test.sh
@@ -3,11 +3,10 @@
 set -e
 set -x
 
-#rm -rf output
-#mkdir output
+rm -rf output
+mkdir output
 
 # Single test
 ~/zkWasm/target/release/cli  -k 18 --function zkmain --output ./output --wasm ./pkg/output.wasm setup
 
 ~/zkWasm/target/release/cli  -k 18 --function zkmain --output ./output --wasm ./pkg/output.wasm single-prove
-~/zkWasm/target/release/cli  -k 18 --function zkmain --output ./output --wasm ./pkg/output.wasm single-verify --proof output/zkwasm.0.transcript.data
